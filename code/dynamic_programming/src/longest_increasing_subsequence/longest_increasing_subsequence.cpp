146c77fb4e76727dbd17cddc185a3daf968c0e77
Ernest <iattempt.net@gmail.com>
2017-12-17 14:48:23 +0800

    fix coding style

--- code/dynamic_programming/src/longest_increasing_subsequence/longest_increasing_subsequence.cpp
+++ code/dynamic_programming/src/longest_increasing_subsequence/longest_increasing_subsequence.cpp
@@ -6,28 +6,40 @@
 using namespace std;
 
 // Bottom-up O(n^2) approach
-int lis(int v[], int n) {
-
+int lis(int v[], int n)
+{
     int dp[n], ans = 0;
-    for(int i = 0; i < n; ++i) {
+
+    for (int i = 0; i < n; ++i)
+    {
         dp[i] = 1;
-        for(int j = 0; j < i; ++j)
-            if(v[j] < v[i])
+
+        for (int j = 0; j < i; ++j)
+            if (v[j] < v[i])
                 dp[i] = max(dp[i], 1 + dp[j]);
+
         ans = max(ans, dp[i]);
     }
+
     return ans;
 }
 
 // Bottom-up O(n*log(n)) approach
-int lis2(int v[], int n) {
+int lis2(int v[], int n)
+{
     // tail[i] stores the value of the lower possible value
     // of the last element in a increasing sequence of size i
     vector<int> tail;
-    for(int i = 0; i < n; ++i) {
+
+    for (int i = 0; i < n; ++i)
+    {
         vector<int>::iterator it = lower_bound(tail.begin(), tail.end(), v[i]);
-        if(it == tail.end()) tail.push_back(v[i]);
-        else *it = v[i];
+
+        if (it == tail.end())
+            tail.push_back(v[i]);
+        else
+            *it = v[i];
     } 
+
     return tail.size();
 }
