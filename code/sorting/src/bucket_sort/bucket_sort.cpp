e64c5c76e60308bef7e085e3b4915327dbaf5bb7
Ernest <iattempt.net@gmail.com>
2018-04-09 16:24:08 +0800

    Fix

--- code/sorting/src/bucket_sort/bucket_sort.cpp
+++ code/sorting/src/bucket_sort/bucket_sort.cpp
@@ -18,24 +18,32 @@ void bucketSort(vector<float>& arr)
 	    b[bi].push_back(arr[i]);
 	}
 
-	// 3) Sort individual buckets
-	for (size_t i=0; i<arr.size(); i++) sort(b[i].begin(), b[i].end());
+    // 2) Put array elements in different buckets
+    for (size_t i = 0; i < arr.size(); i++)
+    {
+        int bi = arr.size() * arr[i];   // Index in bucket
+        b[bi].push_back(arr[i]);
+    }
 
-	// 4) Concatenate all buckets into arr[]
-	int index = 0;
-	for (size_t i = 0; i < arr.size(); i++)
-		for (size_t j = 0; j < b[i].size(); j++)
-		arr[index++] = b[i][j];
+    // 3) Sort individual buckets
+    for (size_t i = 0; i < arr.size(); i++)
+        sort(b[i].begin(), b[i].end());
+
+    // 4) Concatenate all buckets into arr[]
+    int index = 0;
+    for (size_t i = 0; i < arr.size(); i++)
+        for (size_t j = 0; j < b[i].size(); j++)
+            arr[index++] = b[i][j];
 }
 
 /* Driver program to test above funtion */
 int main()
 {
-	vector<float> arr{0.897, 0.565, 0.656, 0.1234, 0.665, 0.3434};
-	bucketSort(arr);
+    vector<float> arr{0.897, 0.565, 0.656, 0.1234, 0.665, 0.3434};
+    bucketSort(arr);
 
-	cout << "Sorted array is \n";
-	for (size_t i=0; i<arr.size(); i++)
-	    cout << arr[i] << " ";
-	return 0;
+    cout << "Sorted array is \n";
+    for (size_t i = 0; i < arr.size(); i++)
+        cout << arr[i] << " ";
+    return 0;
 }
